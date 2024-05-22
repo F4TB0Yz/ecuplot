1877daede5bac9bdde8e27799d9d7b0bd8852e06
F4TB0Yz <jfduarte09@gmail.com>
2024-05-22 15:31:08 -0500

    bugs

--- plugins/user_commands/limites.py
+++ plugins/user_commands/limites.py
@@ -8,33 +8,55 @@ async def startComando(client, message):
     user_name = message.from_user.username
     user_id = message.from_user.id
 
-    mensaje_usuario = message.text[len('/graficar'):]
+    funciones = []
+    mensaje_usuario = message.text[len('/graficar') + 1:]
     mensaje = await filtrarMensaje(mensaje_usuario)
+    for funcion in mensaje:
+        funciones.append(funcion)
+    print(f'mensaje final: {mensaje}')
+    print(f'funciones: {funciones}')
     graficador = Graficador()
-    graficador.graficar(*mensaje)
+    graficador.graficar(*funciones)
 
 
     return await message.reply_photo('output.png')
 
+# async def filtrarMensaje(mensaje):
+#     print(type(mensaje))
+#     mensaje = [funcion.replace(' ', '') for funcion in mensaje]
 
-async def filtrarMensaje(mensaje):
-    print(type(mensaje))
-    mensaje = [funcion.replace(' ', '') for funcion in mensaje]
+#     mensaje_nuevo = ""
+#     for index, caracter in enumerate(mensaje):
+#         if caracter.isdigit() and index + 1 < len(mensaje) and mensaje[index + 1] == 'x':  # Evitar IndexErrors
+#             mensaje_nuevo += caracter + "*x"
+#         else:
+#             mensaje_nuevo += caracter
 
-    mensaje_nuevo = ""
-    for index, caracter in enumerate(mensaje):
-        if caracter.isdigit() and index + 1 < len(mensaje) and mensaje[index + 1] == 'x':  # Evitar IndexErrors
-            mensaje_nuevo += caracter + "*x"
-        else:
-            mensaje_nuevo += caracter
-    if '&' in mensaje_nuevo:
-        mensaje_nuevo = [funcion.split('&') for funcion in mensaje_nuevo]
-    mensaje_nuevo = [funcion.replace(' ', '') for funcion in mensaje_nuevo]
-    mensaje_nuevo = [funcion.replace('^', '**') for funcion in mensaje_nuevo]
-    mensaje_nuevo = [funcion.replace('sqrt', 'np.sqrt') for funcion in mensaje_nuevo]
-    #mensaje = [funcion.replace('+*', '+') for funcion in mensaje]
-    print(type(mensaje_nuevo))
-    return mensaje_nuevo
+#     print(f'mensaje: {mensaje_nuevo}')
+#     mensaje_nuevo = [funcion.replace(' ', '') for funcion in mensaje_nuevo]
+#     mensaje_nuevo = [funcion.replace('^', '**') for funcion in mensaje_nuevo]
+#     mensaje_nuevo = [funcion.replace('sqrt', 'np.sqrt') for funcion in mensaje_nuevo]
+#     #mensaje = [funcion.replace('+*', '+') for funcion in mensaje]
+#     mensaje_nuevo = ''.join(mensaje_nuevo)
+#     mensaje_nuevo.split('&')
+#     print(type(mensaje_nuevo))
+#     print(f'mensaje_nuevo: {mensaje_nuevo}')
+#     return mensaje_nuevo
 
 async def borrarImg():
     os.remove('output.png')
+
+
+async def filtrarMensaje(mensaje):
+    if '^' in mensaje:
+        mensaje = mensaje.replace('^', '**')
+    if 'sqrt' in mensaje:
+        mensaje = mensaje.replace('sqrt', 'np.sqrt')
+    if ' ' in mensaje:
+        mensaje = mensaje.replace(' ', '')
+    if '&' in mensaje:
+        mensaje = mensaje.split('&')
+    else:
+        mensaje = [mensaje]
+    return mensaje
+
